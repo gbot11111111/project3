eaaa17525792821e2c69678f6a037578a79667ac
gbot11111111 <114045371+gbot11111111@users.noreply.github.com>
2026-05-07 17:33:31 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,13 +5,19 @@ let bg_b = 0;
 let bg_fadespeed = 30;
 
 let enemies = [];
+let enemyBullets = [];
 
 function setup() {
   createCanvas(800, 800);
   p = new player();
 
-  ufo = new UFO();
+  ufo = new UFO(372,200);
   enemies.push(ufo);
+}
+
+function preload(){
+  img_alien = loadImage("assets/Aliens.png");
+  img_car = loadImage("assets/CAR.png");
 }
 
 class playerBullet{
@@ -45,11 +51,15 @@ class player{
     this.x = 375;
     this.y = 730;
     this.health = 100;
-    this.armor = 100;
+  }
+
+  damage(number){
+    this.health-=number;
   }
   
   show(){
     rect(this.x,this.y,50);
+    //image(img_alien, this.x, this.y, 50,50);
   }
   
   move(){
@@ -63,27 +73,90 @@ class player{
   
 }
 
-class UFO{
-  constructor(){
-    this.x = 375;
-    this.y = 200;
-    this.health = 100;
+class UFOBullet{
+  constructor(x,y,r){
+    this.x = x;
+    this.y = y;
+    this.r = r;
   }
   
   show(){
-    triangle(
-      this.x,this.y,
-      this.x+50, this.y,
-      this.x+25,this.y+50,50);
+    stroke(0,255,0);
+    noFill();
+    ellipse(this.x,this.y,this.r);
+  }
+  
+  move(){
+    this.y+=10;
+  }
+  
+  touchingPlayer(px, py) {
+    let d = dist(px, py, this.x, this.y);
+    if (d < this.r) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+}
+
+class UFO{
+  constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.health = 100;
+    this.angle = 0;
+    this.dir = 0; 
+    this.size = 100;
+    this.startingy = y;
+  }
+
+  shoot(){
+    // backgroundFlash(0,200,0,15)
+    // let b = new UFOBulletBullet(this.x+25,this.y-25,50);
+    // enemyBullets.push(b);
+  }
+
+  damage(number){
+    this.health-=number;
+    
+    backgroundFlash(0,200,0,15)
+
+    let b = new UFOBullet(this.x+50,this.y+50,50);
+    enemyBullets.push(b);
+
+  }
+  
+  move(){
+    this.angle += 0.05;
+    
+    this.y = sin(this.angle)*100 + this.startingy;
+
+    if (this.dir==0){
+      this.x += 2;
+    }
+
+    if (this.dir==1){
+      this.x -= 2;
+    }
+
+    if (this.x>800-this.size && this.dir == 0){
+      this.dir = 1;
+    }
+
+    if (this.x<0 && this.dir == 1){
+      this.dir = 0;
+    }
+  }
+
+  show(){
+    image(img_alien, this.x, this.y, 100,100);
       
     noStroke();
     fill(255);
     textSize(18);
     text("Health: " + this.health,this.x,this.y-10)
-  }
-  
-  move(){
-
   }
 }
 
@@ -115,8 +188,7 @@ function draw() {
   noStroke();
   fill(255);
   textSize(18);
-  text("Health: " + p.health,50,50)
-  text("Armor: " + p.armor,50,70)
+  text("Health: " + p.health,50,50);
 
   stroke(255);
   noFill();
@@ -130,10 +202,16 @@ function draw() {
 
     for (let j = 0; j < enemies.length; j++) {
 
-      if (playerBullets[i].touchingEnemy(enemies[j].x+25, enemies[j].y+25)){
+      if (playerBullets[i].touchingEnemy(enemies[j].x+enemies[j].size/2, enemies[j].y+enemies[j].size/2)){
         //remove from array
         playerBullets.splice(i,1);
         console.log("hit");
+
+        enemies[j].damage(10);
+
+        if (enemies[j].health == 0){
+          enemies.splice(j,1);
+        }
       }
     }
   }
@@ -143,12 +221,28 @@ function draw() {
     enemies[i].show();
   }
 
+  for (let i = 0; i < enemyBullets.length; i++) {
+    enemyBullets[i].move();
+    enemyBullets[i].show();
+
+      if (enemyBullets[i].touchingPlayer(p.x+25, p.y+25)){
+        //remove from array
+        enemyBullets.splice(i,1);
+        console.log("player hit");
+
+        p.damage(10);
+
+        // if (enemies[j].health == 0){
+        //   enemies.splice(j,1);
+        // }
+      }
+  }
 }
 
 function mousePressed(){
   backgroundFlash(200,0,0,15)
 
-  let pb = new playerBullet(p.x+25,p.y-25,25);
+  let pb = new playerBullet(p.x+25,p.y-25,50);
   playerBullets.push(pb);
 }
 
